4549a4a86bf84b84dea9d6857e8da66e16e36a55
marwanmoub <moubayedmarwancoding@gmail.com>
2025-05-21 13:58:53 +0300

    Finish auth pages

--- src/components/auth/AuthForm.tsx
+++ src/components/auth/AuthForm.tsx
@@ -9,6 +9,8 @@ import {
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import AuthHeader from "./AuthHeader";
+import AuthButton from "./AuthButton";
+import { Link } from "expo-router";
 
 interface AuthParams {
   authType: string;
@@ -28,7 +30,7 @@ export default function AuthForm({ authType = "sign-up" }: AuthParams) {
 
   return (
     <SafeAreaView
-      className="bg-gray-100 flex-1 pt-20 "
+      className="bg-white flex-1 pt-20 "
       style={{
         paddingHorizontal: _paddingHorizontal,
       }}
@@ -36,7 +38,7 @@ export default function AuthForm({ authType = "sign-up" }: AuthParams) {
       <AuthHeader />
 
       <View className="mt-8">
-        <Text className="text-3xl font-bold text-center text-[#5856d6]">
+        <Text className="text-3xl font-bold text-center">
           {authType === "sign-up" ? "Sign Up for GoCami" : "Sign In to GoCami"}
         </Text>
       </View>
@@ -60,7 +62,7 @@ export default function AuthForm({ authType = "sign-up" }: AuthParams) {
                 className="w-full"
               />
             </View>
-            <Pressable className="py-3 bg-[#5856d6] rounded-lg">
+            <Pressable className="py-4 bg-[#5856d6] rounded-lg">
               <Text className="text-center text-white">Continue</Text>
             </Pressable>
           </View>
@@ -96,6 +98,49 @@ export default function AuthForm({ authType = "sign-up" }: AuthParams) {
           </View>
         </>
       )}
+
+      {authType === "sign-in" && <View className="mt-10" />}
+
+      <View className="flex flex-col gap-10">
+        <AuthButton
+          text="Continue with Email"
+          icon="mail-outline"
+          iconColor="black"
+        />
+        {/* <AuthButton text="Sign In with Facebook" icon="logo-google" />
+        <AuthButton text="Sign In with Google" icon="logo-facebook" /> */}
+        <AuthButton
+          text="Continue with Whatsapp"
+          icon="logo-whatsapp"
+          iconColor="green"
+        />
+      </View>
+
+      <View className="bg-gray-100 flex flex-row items-center justify-center absolute bottom-0 left-0 right-0 h-40">
+        {authType === "sign-up" ? (
+          <Text>
+            Already have an account?{" "}
+            <Link
+              href="/auth/signIn"
+              replace
+              className="text-[#5856d6] font-bold"
+            >
+              Log In
+            </Link>
+          </Text>
+        ) : (
+          <Text>
+            Don't have an account?{" "}
+            <Link
+              href="/auth/sign-up"
+              replace
+              className="text-[#5856d6] font-bold"
+            >
+              Sign Up
+            </Link>
+          </Text>
+        )}
+      </View>
     </SafeAreaView>
   );
 }
